c23a3d0e17ceb0dd7466098fe08dbc72bcd5508f
David Pinto <davidpinto15@gmail.com>
2020-07-15 16:10:23 +0100

    changed function to get the date and the message & new csv file to store the dates

---
+++ createdataset.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup as bs
+import csv
+import pandas as pd
+
+def write_to_csv(filename):
+    csvfile = csv.writer(open("chatdata.csv", "w"))
+    csvfile.writerow(["date"])
+    with open(filename) as fp:
+        soup = bs(fp, 'lxml')
+    dates = soup.findAll('div', {'class' : 'uiBoxWhite'})
+    dates = soup.select('div.uiBoxWhite')
+    for date in dates:
+        data = date.select('div')[-1].text
+        data = ''.join(data.split(',')[:2])
+        csvfile.writerow([data])
+
+def count_messages_bydate():
+    df = pd.read_csv('chatdata.csv')
+    df.groupby(df.date).size().to_csv("count.csv", header=True)
+    ab = pd.read_csv('count.csv')
+    ab['date'] = pd.to_datetime(ab.date)
+    ab.sort_values('date').to_csv("sorted.csv", header=True)
--- receive_data.py
+++ receive_data.py
@@ -1,22 +1,42 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from createdataset import *
+#import matplotlib.pyplot as plt
+#from createdataset import *
 import numpy as np
 from bs4 import BeautifulSoup as bs
-import codecs
+import csv
 
-filename = codecs.open("D:/Apps/Google Drive/Code/Python/mesengerAnalysis/facebook-davidmarch15/messages/inbox/antoniosousa_kajqqq3bvq/message_1.html", 'r')
+#Fixo
+filename = open("message_1.html", 'r', encoding='utf8')
+
+#Portatil
+""" filename = codecs.open("D:/Apps/Google Drive/Code/Python/MessengerAnalysis/facebook-davidmarch15/messages/inbox/antoniosousa_kajqqq3bvq/message_1.html", 'r') """
 
 soup = bs(filename, 'lxml')
-text = soup.find_all('div', {'class' : '_3-96 _2let'})
+text = soup.select('div.uiBoxWhite')
+textMsg = soup.find_all('div', {'class' : '_3-96 _2let'})
 
 #Save Logs
-file= open("D:/Apps/Google Drive/Code/Python/mesengerAnalysis/chatdata.txt","a")
+#file= open("chatdata.txt","a")
 
-
+#Get data
 for i in text:
-  msg = i.text
+  date = i.select('div')[-1].text
+  date = ''.join(date.split(',')[:2])
+  #TODO: meter tudo num array ou algo assim e depois mandar esse array pro file
+  df = pd.DataFrame([date], columns=['Date'])
+  df.to_csv('dates.csv')
+  print('date:', date)
+
+#Get messages
+for j in textMsg:
+  msg = j.text
   msg = msg.replace('\n\n', '')
   msg = msg.replace('  ', '')
-  file.write(str(msg) + '\n')
-  print(msg)
+  #print('msg:', msg) 
+
+
+""" df = pd.read_csv('chatdata.csv')
+df.groupby(df.date).size().to_csv("count.csv", header=True)
+ab = pd.read_csv('count.csv')
+ab['date'] = pd.to_datetime(ab.date)
+ab.sort_values('date').to_csv("sorted.csv", header=True) """
