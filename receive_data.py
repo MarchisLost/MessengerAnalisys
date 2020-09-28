c6d83ebd3af62d885dcbc5377224a5bf94d62c60
David Pinto <davidpinto15@gmail.com>
2020-09-28 23:28:50 +0100

    date properly stored on file

--- receive_data.py
+++ receive_data.py
@@ -1,39 +1,40 @@
 import pandas as pd
-#import matplotlib.pyplot as plt
-#from createdataset import *
+
+# import matplotlib.pyplot as plt
+# from createdataset import *
 import numpy as np
 from bs4 import BeautifulSoup as bs
 import csv
 
-#Fixo
-filename = open("message_1.html", 'r', encoding='utf8')
+# Fixo
+filename = open("message_1.html", "r", encoding="utf8")
 
-#Portatil
-""" filename = codecs.open("D:/Apps/Google Drive/Code/Python/MessengerAnalysis/facebook-davidmarch15/messages/inbox/antoniosousa_kajqqq3bvq/message_1.html", 'r') """
+soup = bs(filename, "lxml")
+text = soup.select("div.uiBoxWhite")
+textMsg = soup.find_all("div", {"class": "_3-96 _2let"})
 
-soup = bs(filename, 'lxml')
-text = soup.select('div.uiBoxWhite')
-textMsg = soup.find_all('div', {'class' : '_3-96 _2let'})
-
-#Save Logs
-#file= open("chatdata.txt","a")
-
-#Get data
+# Get data
+dates = []
 for i in text:
-  date = i.select('div')[-1].text
-  date = ''.join(date.split(',')[:2])
-  #TODO: meter tudo num array ou algo assim e depois mandar esse array pro file
-  df = pd.DataFrame([date], columns=['Date'])
-  df.to_csv('dates.csv')
-  print('date:', date)
+    date = i.select("div")[-1].text
+    date = "".join(date.split(",")[:-1])
+    dates.append(date)
+    #print("date:", date)
+#print(dates)    
 
-#Get messages
+#Write on csv
+with open("dates.csv", mode='w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['Date'])
+    for i in dates:
+        writer.writerow([i])
+
+# Get messages
 for j in textMsg:
-  msg = j.text
-  msg = msg.replace('\n\n', '')
-  msg = msg.replace('  ', '')
-  #print('msg:', msg) 
-
+    msg = j.text
+    msg = msg.replace("\n", "")
+    msg = msg.replace("  ", "")
+    #print('msg:', msg)
 
 """ df = pd.read_csv('chatdata.csv')
 df.groupby(df.date).size().to_csv("count.csv", header=True)
